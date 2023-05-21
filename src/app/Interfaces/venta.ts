eccbe2d1fba53a50466689672fd8984703e39313
KennerEspinal <kennerespinalg@gmail.com>
2023-05-20 20:02:21 -0500

    Modified Venta

--- src/app/Interfaces/venta.ts
+++ src/app/Interfaces/venta.ts
@@ -3,6 +3,8 @@ import { DetalleVenta } from "./detalle-venta"
 export interface Venta {
     idVenta?: number,
     numeroDocumento?: string,
+    idCliente?: string,
+    nomCliente?: string,
     tipoPago: string,
     fechaRegistro?: string,
     totalTexto: string,
